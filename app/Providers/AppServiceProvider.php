8769e3206b6d3736ec5a86e554d98521fff14598
Jinzo <elmotrax09@gmail.com>
2025-01-23 00:23:11 +0800

    Revert

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,5 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if($this->app->environment('production')) {
-    \URL::forceScheme('https');
-}
     }
 }
